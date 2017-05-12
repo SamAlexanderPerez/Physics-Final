34a1ef88d84d54c5445a82b4a8071835e63e309a
SamAlexanderPerez <9sperez@stpatrick.org>
2017-05-12 13:55:37 -0500

    Work on just the functions.

---
+++ Physics.js
@@ -0,0 +1,36 @@
+function distance(v,t){
+  let d=v*t;
+  return d;
+}
+function distanceV(d,t){
+  let v=d/t;
+  return v;
+}
+function distanceT(d,v){
+  let t=d/v;
+  return t;
+}
+
+
+function force(m,a){
+  let f=m*a;
+  return f;
+}
+
+function momentum(m,v){
+  let impulse=m*v;
+  return impulse;
+}
+function momentumV(p,m){
+  let v=p/m;
+  return v;
+}
+function momentumM(p,v){
+  let m=p/v;
+  return m;
+}
+
+function ke(m,v){
+  let energy=m*v*v/2;
+  return energy;
+}
