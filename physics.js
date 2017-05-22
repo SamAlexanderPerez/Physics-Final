ef15b2b9ea7b174b86a5dbb9d8dcfd86d505b588
SamAlexanderPerez <9sperez@stpatrick.org>
2017-05-22 13:08:30 -0500

    Done

--- physics.js
+++ physics.js
@@ -26,84 +26,99 @@ function convertToArray(strcma){
     return strcma;
 }
 
-terminal.question("What type of equation are you looking for?",function(str)){
+terminal.question("What type of equation are you looking for?",function(str){
     str.toLowerCase();
     //you will configure your series of questions inside here.
     if(str=="motion"){
-      terminal.question("What variable are you solving for?",function(str)){
+      terminal.question("What variable are you solving for?",function(str){
           str.toLowerCase();
           if(str=="d"){
             printOptions();
-            terminal.question("...",function(str)){
+            terminal.question("...",function(str){
                 str.toLowerCase();
-                convertToArray(strcma);
-                if(str[2]==NaN){
-                    return motion.distance(str[1],str[3]);
+                str=convertToArray(str);
+                if(Number.isNaN(str[2])){
+                    console.log(motion.distance(str[1],str[3]));
+                    end();
                 }
                 else{
-                  return motion.distance2(str[2],str[3],str[1]);
+                  console.log(motion.distance2(str[2],str[3],str[1]));
+                  end();
                 }
             }
 
           }
           else if(str=="v"){
             printOptions();
-            terminal.question("...",function(str)){
+            terminal.question("...",function(str){
                 str.toLowerCase();
-                convertToArray(strcma);
-                if(str[2]==NaN){
-                  return motion.velocity(str[0],str[3]);
+                str=convertToArray(str);
+                if(Number.isNaN(str[2])){
+                  console.log(motion.velocity(str[0],str[3]));
+                  end();
                 }
                 else{
-                  return motion.velocity2(str[1],str[2],str[3]);
+                  console.log(motion.velocity2(str[1],str[2],str[3]));
+                  end();
                 }
               }
             }
           else if(str=="t"){
           printOptions();
-          terminal.question("...",function(str)){
+          terminal.question("...",function(str){
               str.toLowerCase();
-              convertToArray(strcma);
-              if(str[2]==NaN){
-                return motion.time(str[0],str[1]);
+              str=convertToArray(str);
+              if(Number.isNaN(str[2])){
+                console.log(motion.time(str[0],str[1]));
+                end();
               }
             }
           }
         else if(str=="a"){
           printOptions();
-          terminal.question("...",function(str)){
+          terminal.question("...",function(str){
             str.toLowerCase();
-            convertToArray(strcma);
-            return motion.acceleration(str[1],str[3]);
+            str=convertToArray(str);
+            console.log(motion.acceleration(str[1],str[3]));
+            end();
           }
         }
       }
     }
     else if(str=="energy"){
-      terminal.question("What variable are you solving for?",function(str)){
+      terminal.question("What variable are you solving for?",function(str){
           str.toLowerCase();
           if(str=="ke"){
             printOptions();
-            terminal.question("...",function(str)){
+            terminal.question("...",function(str){
                 str.toLowerCase();
-                convertToArray(strcma);
-                return energy.kineticEnergy(str[4],str[1]);
+                str=convertToArray(str);
+                console.log(energy.kineticEnergy(str[4],str[1]));
+                end();
               }
             }
           else if(str=="m"){
             printOptions();
-            terminal.question("...",function(str)){
+            terminal.question("...",function(str){
                 str.toLowerCase();
-                convertToArray(strcma);
-                return energy.mass(str[5],str[1]);
+                str=convertToArray(str);
+                console.log(energy.mass(str[5],str[1]));
+                end();
         }
       }
-        else if(str=="v")
-          
+        else if(str=="v"){
+          printOptions();
+          terminal.question("...",function(str){
+              str.toLowerCase();
+              str=convertToArray(str);
+              console.log(energy.velocity(str[5],str[4]));
+              end();
+        }
+      }
         }
     }
-  }
     else{
-
-    }
-});
+      console.log("I'm sorry,I don't know how to find that.");
+      end();
+  }
+}
